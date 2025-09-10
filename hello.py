17111a1c56252d8ef5503bacd7d0eb8b7bff9904
JihadAlmarbi <almarbyikim135@gmail.com>
2025-09-10 13:22:23 -0800

    make entery point file

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-name = input('Enter your name: \n')
-print('hello ', name)
-age = input ('please enter your age ? \n')
-birth_year = 2025 - int (age)
-print ('you were born in ', birth_year)
+def hello():
+    name = input('Enter your name: \n')
+    print('hello ', name)
+    age = input ('please enter your age ? \n')
+    birth_year = 2025 - int (age)
+    print ('you were born in ', birth_year)
