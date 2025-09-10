15e12e6e042f738a0fa9fac53e4354ac741c83bf
JihadAlmarbi <almarbyikim135@gmail.com>
2025-09-10 13:45:59 -0800

    add programm to choice the preson who buy the dinner

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from hello import hello
+from rand import master
 def main():
-    hello()
+    # hello()
+    print("the dinner on ",master())
 if "__main__" == __name__:
     main()
---
+++ rand.py
@@ -0,0 +1,9 @@
+from random import(
+    random,
+    randint,
+    choice,
+)
+friends = ["Mohammed","Ahmed", "Jihad"]
+def master():
+    chosen = choice(friends)
+    return chosen
