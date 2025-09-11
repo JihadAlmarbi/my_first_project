208e06be6b0b1b1916a7ff832275fef3da89cf84
JihadAlmarbi <almarbyikim135@gmail.com>
2025-09-10 16:07:35 -0800

    m

--- students/levels.py
+++ students/levels.py
@@ -1,3 +1,10 @@
+class department:
+    def __init__(self):
+        self.levels = list()
+    def y(self, other,x,y):
+        level = other( x, y)
+        self.levels.append(level)
+
 class levels:
     def __init__(self, level, term):
         self.level = level
@@ -11,12 +18,46 @@ class students:
         self.name = name 
         self.age = age 
 
-l = levels("first level","second term")
-l.x(students, 'ali',19)
-l.x(students, 'saleh',19)
-l.x(students, 'khaled',19)
-l.x(students, 'ahmed',19)
-l.x(students, 'mohammed',19)
+m = department()
+m.y(levels,'one','seconde term')
+m.y(levels,'two','first term')
+m.y(levels,'three','first term')
+m.y(levels,'four','first term')
+m.y(levels,'five','first term')
+m.y(levels,'six','first term')
 
-for n in l.student:
-    print(n.name)
+m.levels[0].x(students,'a',12)
+m.levels[0].x(students,'b',12)
+m.levels[0].x(students,'c',12)
+m.levels[0].x(students,'d',12)
+m.levels[0].x(students,'e',12)
+
+m.levels[1].x(students,'f',12)
+m.levels[1].x(students,'g',12)
+m.levels[1].x(students,'h',12)
+m.levels[1].x(students,'i',12)
+m.levels[1].x(students,'j',12)
+m.levels[1].x(students,'k',12)
+
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+m.levels[2].x(students,'k',12)
+
+
+
+for i in m.levels:
+    print ('\n___________\n')
+    print (i.level)
+    counter = 0
+    for j in i.student:
+        counter +=1
+        print ('\n______________')
+        print ("student ",counter , ':')
+        print ("name ",j.name)
+        print ("age:",j.age)
